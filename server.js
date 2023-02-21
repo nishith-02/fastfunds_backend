7a5158f5783c53ebd85f444588610e81eae87cf4
nishith-02 <sainishith02.varaganti@gmail.com>
2023-02-21 09:07:31 +0530

    document viewing

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const cors = require("cors");
@@ -6,6 +7,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 app.use(cors());
+app.use("/documents",express.static(path.join(__dirname, "documents")));
 app.use(express.json());
 app.use(express.json({ limit: "100000000" }));
 
